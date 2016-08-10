98bce9715fe04bbbfa3634f988bd0e5205a3884e
Oliver Lockwood <oliver.lockwood@cantab.net>
2016-08-10 18:56:21 +0100

    Simplify schema migration operation

--- src/main/java/com/oliverlockwood/cqlmigrate/example/ExampleApplication.java
+++ src/main/java/com/oliverlockwood/cqlmigrate/example/ExampleApplication.java
@@ -5,12 +5,8 @@ import com.oliverlockwood.cqlmigrate.example.config.ExampleAppConfiguration;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ExampleApplication extends Application<ExampleAppConfiguration> {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleApplication.class);
 
     public static void main(String[] args) throws Exception {
         new ExampleApplication().run(args);
